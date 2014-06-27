159de04f4431c4fe58a28d2dd88ecfa73dcdc642
Patrick Herrmann <patrickwherrmann@gmail.com>
2014-06-26 20:55:27 -0700

    Add comments

--- Cards.hs
+++ Cards.hs
@@ -1,5 +1,6 @@
 module Cards where
 
+--Card ranks; Aces are high
 data Rank = Two
           | Three
           | Four
@@ -15,27 +16,32 @@ data Rank = Two
           | Ace
    deriving (Show, Eq, Ord, Enum)
 
+--Card suits
 data Suit = Clubs
           | Diamonds
           | Hearts
           | Spades
-   deriving (Show, Eq, Ord, Enum)
+   deriving (Show, Eq, Enum)
 
+--Playing cards; no jokers
 data Card = Card Rank Suit
 
 instance Show Card where
    show (Card r s) = (show r) ++ " of " ++ (show s)
 
+--Provides a full deck
 fullDeck = [Card r s | r <- [Two .. Ace], s <- [Clubs .. Spades]]
 
+--Different names for a list of cards
 type Pile = [Card]
 type Deck = Pile
+type Hand = Pile
 
---Shorthand for shorter printing of cards
-
+--Shorthand is an abridged string representation
 class Shorthand a where
    shorthand :: a -> String
 
+--One character representation
 instance Shorthand Rank where
   shorthand Two   = "2"
   shorthand Three = "3"
@@ -47,8 +53,10 @@ instance Shorthand Rank where
   shorthand Nine  = "9"
   shorthand r     = take 1 . show $ r
 
+--One character representation
 instance Shorthand Suit where
   shorthand = take 1 . show
 
+--Two character representation
 instance Shorthand Card where
   shorthand (Card r s) = (shorthand r) ++ (shorthand s)
